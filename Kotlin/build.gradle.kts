847080e6bb3c131f4eabf60003876896d87f0446
IanSeun <temahianseunsamuel.ndolo@ictuniversity.edu.cm>
2026-03-07 03:08:51 +0100

    functional test

---
+++ Kotlin/build.gradle.kts
@@ -0,0 +1,10 @@
+// Top-level build file
+plugins {
+    alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.kotlin.android)      apply false
+    alias(libs.plugins.kotlin.compose)      apply false
+    alias(libs.plugins.hilt.android)        apply false
+    alias(libs.plugins.ksp)                 apply false
+    alias(libs.plugins.kotlinMultiplatform) apply false
+    alias(libs.plugins.composeMultiplatform) apply false
+}
