f57d34dd79b8085642a200251ad26595069fefe1
IanSeun <160847124+IanSeun@users.noreply.github.com>
2026-03-18 08:52:46 +0100

    Delete Kotlin/app directory

--- Kotlin/app/src/commonMain/kotlin/com/g/gradeapp/data/local/DataStore.kt
+++
@@ -1,6 +0,0 @@
-package com.g.gradeapp.data.local
-
-import androidx.datastore.core.DataStore
-import androidx.datastore.preferences.core.Preferences
-
-expect fun createDataStore(path: String): DataStore<Preferences>
--- Kotlin/app/src/main/java/com/g/gradeapp/GradeApplication.kt
+++
@@ -1,8 +0,0 @@
-// app/src/main/java/com/g/gradeapp/GradeApplication.kt
-package com.g.gradeapp
-
-import android.app.Application
-import dagger.hilt.android.HiltAndroidApp
-
-@HiltAndroidApp
-class GradeApplication : Application()
